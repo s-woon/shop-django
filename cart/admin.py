f8b428a1d1cf6054ceca615e37d9310bf7d59da4
s-woon <sus9763@gmail.com>
2022-02-04 18:34:06 +0900

    A commit

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CartItem
 
 # Register your models here.
+@admin.register(CartItem)
+class CartItem(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'product',
+        'quantity',
+        'created_at',
+    ]
