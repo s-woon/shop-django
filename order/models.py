c75ca98ca5908ea83e07b1ef1a99a9a714a010bd
s-woon <sus9763@gmail.com>
2022-02-11 16:59:03 +0900

    edit kakaopay success

--- order/models.py
+++ order/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Order(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name='사용자')
+    partner_order_id = models.CharField(max_length=250, default='')
+    tid = models.CharField(max_length=250, default='')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='사용자')
     product = models.ForeignKey('product.Product', on_delete=models.CASCADE, verbose_name='상품')
     quantity = models.IntegerField(verbose_name='수량')
     register_date = models.DateTimeField(auto_now_add=True, verbose_name='주문시간')
+    amount = models.DecimalField(max_digits=10, decimal_places=0, verbose_name='주문금액', default=0)
 
     class Meta:
         db_table = 'consumer_order'
--- order/views.py
+++ order/views.py
@@ -6,15 +6,31 @@ import json
 import requests
 from cart.models import CartItem
 from product.models import Product
+from order.models import Order
 # Create your views here.
 
 
-def kakaopay(request, uid):
+def kakaopay(request, uid, amount=0, totalQuantity=0):
     cartitem = CartItem.objects.filter(user_id=uid)
+    order = Order
+    items = []
+    # 총합가격
+    for subtotal in cartitem:
+        amount += (subtotal.product.price * subtotal.quantity)
+    # 총합갯수
+    for subtotal in cartitem:
+        totalQuantity += subtotal.quantity
+    # 상품명
     for item in cartitem:
-        print(item.user)
-        print(item.product)
-        print(item.quantity)
+        items.append(item.product.name)
+
+
+    id = order.objects.latest('id')
+    if id == None:
+        id = 0
+
+    partner_order_id = id + 1
+
     url = 'https://kapi.kakao.com/v1/payment/ready'
     headers = {
         'Authorization': 'KakaoAK ',
@@ -22,11 +38,11 @@ def kakaopay(request, uid):
     }
     data = {
         'cid': "TC0ONETIME",
-        'partner_order_id': "1",
-        'partner_user_id': "german",
-        'item_name': "product",
-        'quantity': 1,
-        'total_amount': 100,
+        'partner_order_id': partner_order_id,
+        'partner_user_id': cartitem.user.username,
+        'item_name': items,
+        'quantity': totalQuantity,
+        'total_amount': amount,
         'tax_free_amount': 0,
         'approval_url': 'http://127.0.0.1:8080/order/kakaopay/success/',
         'fail_url': 'http://127.0.0.1:8080/order/kakaopay/cancel/',
@@ -38,13 +54,34 @@ def kakaopay(request, uid):
     result = response.json()
     request.session['tid'] = result['tid']
     next_url = result['next_redirect_pc_url']
-    return redirect(next_url)
 
-    # return render(request, 'kakaopay/notpost.html')
+    # DB 저장
+    order.user = cartitem.user
+    return redirect(next_url)
 
 
 def kakaosuccess(request):
-    return render(request, 'kakaopay/success.html')
+    print(request.session.keys())
+    URL = 'https://kapi.kakao.com/v1/payment/approve'
+    headers = {
+        "Authorization": "KakaoAK ",
+        "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+    }
+    params = {
+        "cid": "TC0ONETIME",  # 테스트용 코드
+        "tid": request.session['tid'],  # 결제 요청시 세션에 저장한 tid
+        "partner_order_id": "1",  # 주문번호
+        "partner_user_id": "german",  # 유저 아이디
+        "pg_token": request.GET.get("pg_token"),  # 쿼리 스트링으로 받은 pg토큰
+    }
+    res = requests.post(URL, headers=headers, params=params)
+    amount = res.json()['amount']['total']
+    res = res.json()
+    context = {
+        'res': res,
+        'amount': amount,
+    }
+    return render(request, 'kakaopay/success.html', context)
 
 
 def kakaocancel(request):
